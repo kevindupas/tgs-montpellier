ced37c0cb7b852375d46f585eb53f2636695c6b7
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2023-03-09 19:13:21 +0100

    add image

--- src/Home/components/AllSalon.jsx
+++ src/Home/components/AllSalon.jsx
@@ -48,12 +48,12 @@ export default function AllSalon() {
     } if (allSalon) {
         return (
 
-            <div className="bg-[url('/dist/images/events.png')] bg-cover">
+            <div className="bg-[url('/dist/images/fond1.jpg')] bg-cover">
                 <div className="mx-auto max-w-7xl py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24">
                     <div className="space-y-8 sm:space-y-12">
                         <div className="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl">
                             <h2 className="text-4xl text-black font-bold font-heading_bold tracking-[0.566667px] leading-[.95] uppercase">Nos events</h2>
-                            <p className="text-xl text-gray-500 font-bold">Retrouvez nos autres salons.</p>
+                            <p className="text-xl text-gray-700 font-bold">Retrouvez nos autres salons.</p>
                         </div>
                         <div className="mx-auto grid grid-cols-3 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-3">
                             {allSalon.map((value) => {
