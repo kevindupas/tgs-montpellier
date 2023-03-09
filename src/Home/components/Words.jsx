1f50b6740e7130d3bc445c8037d37f049a3da387
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2023-03-09 20:08:00 +0100

    Update css Word Component

--- src/Home/components/Words.jsx
+++ src/Home/components/Words.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Words() {
     return (
-        <section className="pt-[20px] pb-[20px] lg:pt-[50px] lg:pb-[50px] overflow-hidden bg-black">
+        <section className="pt-[20px] pb-[20px] lg:pt-[50px] lg:pb-[50px] overflow-hidden bg-zinc-900">
             <div className="max-w-[1200px] w-[calc(100%-30px)] mx-auto">
                 <ul className="flex justify-center flex-wrap ml-[-75px]">
                     <li className="mt-[17px] lg:mt-[23px] ml-[75px]">
