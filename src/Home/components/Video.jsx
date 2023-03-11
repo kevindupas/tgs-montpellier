a698ab4bd479ca421f59006a6ce2c8d76002fc71
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2023-03-11 18:35:04 +0100

    update css

--- src/Home/components/Video.jsx
+++ src/Home/components/Video.jsx
@@ -10,8 +10,8 @@ export default function Video({
 
     return (
         <section className="relative h-[calc(100vh-80px)]">
-            <div className="relative z-10 max-w-[1200px] mx-auto h-full content-end flex items-center flex-row-reverse">
-                <h1 className="text-white font-heading_bold tracking-[0.566667px] leading-[.95] indent-0 uppercase ml-12 lg:mr-0 text-5xl lg:text-6xl fix-width">
+            <div className="absolute lg:right-0 bottom-48 lg:bottom-12 z-20">
+                <blockquote className="text-white font-heading_bold tracking-[0.566667px] leading-[.95] indent-0 uppercase text-5xl lg:text-5xl fix-width border-l-[15px] border-orange-500 pl-5 py-4 relative">
                     <span
                         className="drop-shadow-2xl"
                         style={{
@@ -22,20 +22,19 @@ export default function Video({
                     >
                         {settings.event_date}
                     </span>
-                    <br />
-                </h1>
+                </blockquote>
+            </div>
 
-                <div className="absolute w-20 lg:w-[216px] left-3 lg:left-0 -bottom-14 lg:bottom-[-53px] z-10">
-                    <img
-                        style={{
-                            transform:  'translate(0px, 0px)',
-                            opacity:    '1',
-                            visibility: 'inherit',
-                        }}
-                        src="/dist/images/icons-test.png"
-                        alt=""
-                    />
-                </div>
+            <div className="absolute w-20 lg:w-[216px] left-3 lg:left-60 -bottom-14 lg:bottom-[-53px] z-10">
+                <img
+                    style={{
+                        transform:  'translate(0px, 0px)',
+                        opacity:    '1',
+                        visibility: 'inherit',
+                    }}
+                    src="/dist/images/icons-test.png"
+                    alt=""
+                />
             </div>
             <div
                 className="absolute bottom-0 left-0 mx-auto top-0 right-[calc(50%-150px)] md:right-[calc(50%-300px)] lg:right-[calc(50%-516px)]"
